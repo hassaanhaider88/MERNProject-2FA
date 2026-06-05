0bf52e34660b6cb3b851815080e94e91fc8136d5
hassaanhaider88 <hassaanhaider088@gmail.com>
2026-06-05 06:48:06 +0500

    adding basic setpu

--- src/index.js
+++ src/index.js
@@ -1,18 +1,37 @@
-import express from 'express';
 import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import session from 'express-session';
+import passport from 'passport';
 
 dotenv.config();
 
 const app = express();
-
-
-// Middleware
-app.use(express.json({limit : '10mb'})); // Increase the limit to 10mb
-app.use(express.urlencoded({ extended: true }));
 const PORT = process.env.PORT || 5000;
 
+// Middleware
+const corsOptions = {
+    origin: ["http://localhost:3001"],
+    credential: true
+}
+app.use(cors(corsOptions));
+app.use(express.json({ limit: '10mb' })); // Increase the limit to 10mb
+app.use(express.urlencoded({ extended: true, limit: '100mb' }));
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false, maxAge: 24 * 60 * 60 * 1000 }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+
+
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.json({ success: true, message: '2 FA Auth Server is running' });
 });
 
 
